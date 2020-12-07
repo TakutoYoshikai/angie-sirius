749d5e1d4eaa6d64ef2f6565cb9483d0f7a24cd5
lucky <takuto.yoshikai@gmail.com>
2020-12-07 22:30:58 +0900

    setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'angie-sirius',
+    version = '1.0.0',
+    url = 'https://github.com/TakutoYoshikai/cad.git',
+    license = 'MIT LICENSE',
+    author = 'Takuto Yoshikai',
+    author_email = 'takuto.yoshikai@gmail.com',
+    description = "image format changer",
+    install_requires = ['setuptools', "lina@git+https://github.com/TakutoYoshikai/lina.git", "cad@git+https://github.com/TakutoYoshikai/cad.git", "parade@git+https://github.com/TakutoYoshikai/parade.git", "sirius@git+https://github.com/TakutoYoshikai/sirius.git"],
+    packages = find_packages(),
+    entry_points={
+        "console_scripts": [
+            "angie-sirius = angie-sirius.angie-sirius:main",
+        ]
+    }
+)
